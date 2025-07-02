bf4f0ce2c6f7c95f512102fb0ab33b6c7685aa07
Pierrick <pierrickhauguel@outlook.com>
2025-07-02 12:05:05 +0200

    solution 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,58 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { getPlanets } from "./services/planet";
+
+type Planet = {
+  name: string;
+  uid: string;
+  url: string;
+};
+
+type PlanetsResponse = {
+  results: Array<Planet>;
+  next: string;
+  previous: string;
+};
 
 function App() {
-  const [planets, setPlanets] = useState([]);
-
-  const [refetch, setRefetch] = useState(false);
-
-  const handleRefetch = () => {
-    setRefetch(!refetch);
-  };
+  const [planetsResponse, setPlanetsResponse] = useState<PlanetsResponse>({
+    results: [],
+    next: "",
+    previous: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    fetch("https://www.swapi.tech/api/planets")
-      .then((response) => response.json())
+    getData(`https://www.swapi.tech/api/planets?page=1&limit=10`);
+  }, []);
+
+  const getData = (url: string) => {
+    setLoading(true);
+    getPlanets(url)
       .then((data) => {
-        setPlanets(data.results);
+        setPlanetsResponse(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
       });
-  }, [refetch]);
+  };
 
   return (
     <div>
-      <button onClick={handleRefetch}>Refetch</button>
-      {planets.map((planet) => (
+      {loading && <div>Loading...</div>}
+      {error && <div>Error</div>}
+      {planetsResponse.results.map((planet) => (
         <div>{planet.name}</div>
       ))}
+      <button onClick={() => getData(planetsResponse.previous)}>
+        Previous
+      </button>
+      <button onClick={() => getData(planetsResponse.next)}>Next</button>
     </div>
   );
 }
